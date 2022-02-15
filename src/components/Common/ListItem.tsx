8d025651547d06ec4939fde4f9c2b194c61237ca
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2022-02-15 21:54:49 +0530

    show icons only on hover increased paddingY on item

--- src/components/Common/ListItem.tsx
+++ src/components/Common/ListItem.tsx
@@ -11,11 +11,11 @@ interface ListItemProps {
 const ListItem: React.FC<ListItemProps> = ({name, index,onDeleteIconClicked}) => {
 
     return (
-        <Row className="list-item d-flex mx-0">
+        <Row className="list-item d-flex mx-0 py-1">
             <Col className="align-items-center ps-0" xs={9}>
                 <span className="">{index+1}. {name}</span>
             </Col>
-            <Col className="d-flex justify-content-end align-items-center" xs={3}>
+            <Col className="d-none justify-content-end align-items-center list-item-icons" xs={3}>
                 <Edit className="d-inline-block pe-sm-1 edit-icon me-2"/>
                 <Trash2 className="d-inline-block pe-sm-1 delete-icon" onClick={() => {
                     onDeleteIconClicked(index);
