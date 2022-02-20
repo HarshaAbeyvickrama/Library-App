c76534dde50d1a86d66c5dc3faac2b7ca5ed5abc
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2022-02-20 12:03:58 +0530

    created directories for sections in scss partials

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.scss';
-import { Col, Container, Row } from "react-bootstrap";
-import Welcome from './components/Welcome/Welcome';
 import Library from "./views/Library";
 
 function App() {
